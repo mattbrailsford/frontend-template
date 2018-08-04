343348cdbeb763eb87f105a9df791e1dca6333a6
mattbrailsford <me@mattbrailsford.com>
2018-08-04 20:37:58 +0100

    Added more comments

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
-// Globally register all components
+// Globally register all components as we won't know if / when a component
+// will be used as it'll be decided by the CMS
 // taken from https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
 const requireComponent = require.context('./components', true, /.vue$/);
 
@@ -17,6 +18,9 @@ requireComponent.keys().forEach(fileName => {
 })
 
 // Start the vue app
+// The only job of this instance is to "reactify" the page
+// and make our components work, but for the most part,
+// it shouldn't really do anything
 window.vm = new Vue({
   el: '#app'
 })
