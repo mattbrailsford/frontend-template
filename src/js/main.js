bf61dfbc9ab4314681eed8af768a75671e7324cd
mattbrailsford <me@mattbrailsford.com>
2018-08-05 08:56:17 +0100

    Added convention that component files names beginning with '_' should not be registered as global

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
-// Globally register all components as we won't know if / when a component
-// will be used as it'll be decided by the CMS
+// Globally register all components by default as we won't know if / when a component
+// will be used as it'll be decided by the CMS. If you don't want it globally registerd
+// then prefix the filename with a '_'
 // taken from https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
-const requireComponent = require.context('./components', true, /.vue$/);
+const requireComponent = require.context('./components', true, /(?:^|\/)(?!_)\w+\.vue$/);
 
 // For each matching file name...
 requireComponent.keys().forEach(fileName => {
